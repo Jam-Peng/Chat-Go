207746f5ab0f7b4b8a0111f7506432d52a4ed8f1
Jam Peng | 彭喆 <95309839+Jam-Peng@users.noreply.github.com>
2023-09-22 04:24:50 +0800

    Delete chatroom directory

--- chatroom/staticfiles/js/script.js
+++
@@ -1,8 +0,0 @@
-const dropdownButton = document.querySelector("#dropdown-button");
-const dropdownMenu = document.querySelector(".dropdown-menu");
-
-if (dropdownButton) {
-  dropdownButton.addEventListener("click", (e) => {
-    dropdownMenu.classList.toggle("show");
-  });
-}
